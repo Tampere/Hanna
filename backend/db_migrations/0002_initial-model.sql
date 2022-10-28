1d8abdb0265f811acd5f7452da66dffe48d5445e
Tuukka Kataja <tuukka.kataja@ubigu.fi>
2022-10-28 14:10:44 +0300

    [projects] WIP adding and listing projects

--- backend/db_migrations/0002_initial-model.sql
+++ backend/db_migrations/0002_initial-model.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS app.project (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-  description text NOT NULL,
   project_name text NOT NULL,
+  description text NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL CHECK (start_date < end_date),
   created_at TIMESTAMPTZ DEFAULT NOW(),
   geom geometry
 );
