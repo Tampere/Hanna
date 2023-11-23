5f4786c8e5fb3f1aa668cfe9eda09007bed4555d
Janne Peltonen <janne.peltonen@ubigu.fi>
2023-11-23 14:19:35 +0200

    Implement and update Management layouts

--- backend/src/router/userPermissions.ts
+++ backend/src/router/userPermissions.ts
@@ -2,6 +2,7 @@ import { TRPCError } from '@trpc/server';
 
 import { logger } from '@backend/logging';
 import { TRPC } from '@backend/router';
+import { investmentProjectFragment } from '@backend/components/project/search';
 
 export const createUserPermissionsRouter = (t: TRPC) => {
   const baseProcedure = t.procedure.use(async (opts) => {
@@ -24,6 +25,36 @@ export const createUserPermissionsRouter = (t: TRPC) => {
           userName: 'Test User',
           isAdmin: false,
           permissions: ['investmentProject.write', 'detailplanProject.write', 'financials.write'],
+        },
+        {
+          userId: '12346',
+          userName: 'Test User 2',
+          isAdmin: true,
+          permissions: ['investmentProject.write', 'detailplanProject.write', 'financials.write'],
+        },
+        {
+          userId: '12347',
+          userName: 'Test User 3',
+          isAdmin: true,
+          permissions: ['detailplanProject.write', 'financials.write'],
+        },
+        {
+          userId: '12348',
+          userName: 'Test User 4',
+          isAdmin: false,
+          permissions: ['investmentProject.write', 'detailplanProject.write'],
+        },
+        {
+          userId: '12349',
+          userName: 'Test User 6',
+          isAdmin: false,
+          permissions: ['investmentProject.write', 'detailplanProject.write', 'financials.write'],
+        },
+        {
+          userId: '12350',
+          userName: 'Test User 5',
+          isAdmin: false,
+          permissions: ['investmentProject.write', 'financials.write'],
         },
       ];
     }),
--- frontend/src/views/Management/UserPermissions/index.tsx
+++ frontend/src/views/Management/UserPermissions/index.tsx
@@ -1,15 +1,121 @@
-import { Box } from '@mui/material';
+import { Box, Button, Checkbox, InputAdornment, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography, css } from '@mui/material';
 
 import { trpc } from '@frontend/client';
+import { SaveSharp, SaveTwoTone, SearchTwoTone } from '@mui/icons-material';
+import { useTranslations } from '@frontend/stores/lang';
+
+
+type Permission = 'investmentProject.write' | 'detailplanProject.write' | 'financials.write';
+
+type User = {
+  userId: string,
+  userName: string,
+  isAdmin: boolean,
+  permissions: Permission[],
+}
 
 export function UserPermissionsPage() {
-  const userPermissions = trpc.userPermissions.getAll.useQuery();
+  const tr = useTranslations();
+  const query = trpc.userPermissions.getAll.useQuery();
+
+  if ( query === undefined || query.data === undefined ){
+    return;
+  }
+
+  let userPermissions = query.data.sort((a, b) => {
+    if (a.isAdmin !== b.isAdmin) {
+      return a.isAdmin ? -1 : 1; // List the admins first
+    } else {
+      return a.userName.localeCompare(b.userName);
+    }
+  }).map( (user) => {
+    return {
+      userId: user.userId,
+      userName: user.userName,
+      isAdmin: user.isAdmin,
+      permissions: user.permissions,
+    }
+  });
 
   return (
-    <Box>
-      <h1>UserPermissionsPage</h1>
-
-      <pre>{JSON.stringify(userPermissions.data, null, 2)}</pre>
+    <Box
+      css={css`
+      padding: 16px;
+      `}
+    >
+      <Box
+        css={css`
+          display: flex;
+          justify-content: space-between;
+          margin-bottom: 24px;
+          align-items: center;
+        `}
+      >
+        <Typography variant="h4" component="h1">{tr('management.tabs.userPermissions')}</Typography>
+        <Box
+          css={css`
+            display: flex;
+            gap: 8px;
+            height: max-content;
+          `}
+        >
+          <Button
+            variant="contained"
+            endIcon={<SaveSharp />}
+            disabled
+          >
+            {tr('genericForm.saveChanges')}
+          </Button>
+        </Box>
+      </Box>
+      <TextField
+        variant="outlined"
+        size="medium"
+        fullWidth={true}
+        placeholder={tr('userPermissions.filterPlaceholder')}
+        InputProps={{startAdornment: (
+        <InputAdornment position="start">
+          <SearchTwoTone />
+        </InputAdornment>)}}
+      />
+      <TableContainer
+        css={css`margin-top: 32px;`}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                {tr('userPermissions.userName')}
+              </TableCell>
+              <TableCell align="center">
+                {tr('userPermissions.investmentProject.write')}
+              </TableCell>
+              <TableCell align="center">
+                {tr('userPermissions.detailplanProject.write')}
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userPermissions.map( (user) => (
+              <TableRow key={user.userId} hover={true}>
+                <TableCell component="th" variant="head" scope="row">
+                  {user.userName}
+                  {user.isAdmin && (
+                    <Typography variant="body2" component="span" css={css`opacity: 0.7`}>
+                      &nbsp;({tr('userPermissions.admin')})
+                    </Typography>
+                  )}
+                </TableCell>
+                <TableCell align="center">
+                  <Checkbox checked={user.isAdmin || user.permissions.includes('investmentProject.write')} disabled={user.isAdmin} />
+                </TableCell>
+                <TableCell align="center">
+                  <Checkbox checked={user.isAdmin || user.permissions.includes('detailplanProject.write')} disabled={user.isAdmin} />
+                </TableCell>
+              </TableRow>)
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
-  );
+  )
 }
