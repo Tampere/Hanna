3b46f3e1ce8dd2e10121cf1bd5fd3a93df0fc28a
Tuukka Kataja <tuukka.kataja@ubigu.fi>
2023-11-28 14:42:41 +0200

    Add project permission migration

---
+++ backend/db_migrations/0059_project_permissions.sql
@@ -0,0 +1,6 @@
+CREATE TABLE project_permission (
+    project_id uuid NOT NULL REFERENCES app.project(id) ON DELETE CASCADE,
+    user_id text NOT NULL REFERENCES app.user(id) ON DELETE CASCADE,
+    can_write boolean NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (project_id, user_id)
+);
