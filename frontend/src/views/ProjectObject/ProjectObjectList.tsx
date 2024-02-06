00997c0271f587fe5ba76730697c8dfe83269dd3
Mikael Moilanen <mikael.moilanen@gmail.com>
2024-02-06 16:37:08 +0200

    Restyle project object list

--- frontend/src/views/ProjectObject/ProjectObjectList.tsx
+++ frontend/src/views/ProjectObject/ProjectObjectList.tsx
@@ -1,10 +1,11 @@
 import { AddCircle, NavigateNext } from '@mui/icons-material';
 import { Box, Button, Card, CardActionArea, List, Typography, css } from '@mui/material';
 import dayjs from 'dayjs';
+import { useAtomValue } from 'jotai';
 import { Link } from 'react-router-dom';
 
 import { trpc } from '@frontend/client';
-import { useTranslations } from '@frontend/stores/lang';
+import { langAtom, useTranslations } from '@frontend/stores/lang';
 import { ProjectTypePath } from '@frontend/types';
 
 interface Props {
@@ -26,6 +27,16 @@ const cardStyle = css`
 
 export function ProjectObjectList(props: Props) {
   const tr = useTranslations();
+  const lang = useAtomValue(langAtom);
+  const codes = trpc.code.get.useQuery(
+    { codeListId: 'KohteenLaji', allowEmptySelection: false },
+    { staleTime: Infinity },
+  );
+
+  function getObjectStageTextById(objectId: string) {
+    return codes?.data?.find((code) => code.id.id === objectId)?.text[lang] ?? '';
+  }
+
   const projObjects = trpc.projectObject.getByProjectId.useQuery({ projectId: props.projectId });
 
   return (
@@ -59,14 +70,36 @@ export function ProjectObjectList(props: Props) {
             >
               <Card variant="outlined" css={cardStyle}>
                 <NavigateNext sx={{ color: '#aaa', mr: 1 }} />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '0.15rem',
+                  }}
+                >
                   <Typography sx={{ lineHeight: '120%' }} variant="button">
                     {projObj.objectName}
                   </Typography>
+
                   <Typography sx={{ lineHeight: '120%' }} variant="overline">
                     {dayjs(projObj.startDate).format(tr('date.format'))} —{' '}
                     {dayjs(projObj.endDate).format(tr('date.format'))}
                   </Typography>
+                  <span
+                    css={css`
+                      padding: 2px 6px;
+                      font-size: x-small;
+                      font-weight: 500;
+                      color: #333;
+                      border-radius: 8px;
+                      background-color: ${projObj.objectStage === '01' ? '#91c9ea' : '#f1eeeb'};
+                      position: absolute;
+                      bottom: 0.5rem;
+                      right: 1rem;
+                    `}
+                  >
+                    {getObjectStageTextById(projObj.objectStage)}
+                  </span>
                 </Box>
               </Card>
             </CardActionArea>
