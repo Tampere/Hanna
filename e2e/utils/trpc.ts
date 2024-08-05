d5323800ff6b8da9d8101a47f191cc988b68475e
Mikael Moilanen <mikael.moilanen@gmail.com>
2024-08-05 14:37:46 +0300

    E2e to use esm

--- e2e/utils/trpc.ts
+++ e2e/utils/trpc.ts
@@ -1,12 +1,12 @@
 import { createTRPCProxyClient } from '@trpc/client';
 import { httpLink } from '@trpc/client/links/httpLink';
 import { Agent } from 'https';
-import nodeFetch from 'node-fetch';
+import nodeFetch, { RequestInit } from 'node-fetch';
 import { setDefaultResultOrder } from 'node:dns';
 import { Cookie, Page } from 'playwright';
 import superjson from 'superjson';
 
-import type { AppRouter } from '../../backend/src/router';
+import type { AppRouter } from '../../backend/src/router/index.js';
 
 // In Node version >= 17 localhost is resolved with IPv6 rather than IPv4 - revert this back to normal to make Caddy work properly
 setDefaultResultOrder('ipv4first');
@@ -25,14 +25,14 @@ export async function createTRPCClient(page: Page) {
       httpLink({
         url: 'https://localhost:1443/trpc',
         fetch(url, options) {
-          return nodeFetch(url, {
+          return nodeFetch(url.toString(), {
             ...options,
             agent,
             headers: {
               ...options.headers,
               cookie: getCookieHeaderValue(cookies),
             },
-          });
+          } as RequestInit);
         },
       }),
     ],
