852567bbb342d70fa7dcf44d155979383d58c602
Mikael Moilanen <mikael.moilanen@gmail.com>
2024-11-26 17:08:03 +0200

    Add start year filter for project object list

--- frontend/src/views/ProjectObject/ProjectObjectList.tsx
+++ frontend/src/views/ProjectObject/ProjectObjectList.tsx
@@ -15,7 +15,7 @@ import {
 } from '@mui/material';
 import dayjs from 'dayjs';
 import { useAtomValue } from 'jotai';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { trpc } from '@frontend/client';
@@ -45,7 +45,10 @@ const cardStyle = css`
 export function ProjectObjectList(props: Props) {
   const tr = useTranslations();
   const lang = useAtomValue(langAtom);
+
   const [orderBy, setOrderBy] = useState<DbObjectOrderBy>('name');
+  const [byYear, setByYear] = useState<number | 'all'>('all');
+
   const codes = trpc.code.get.useQuery(
     { codeListId: 'KohteenLaji', allowEmptySelection: false },
     { staleTime: Infinity },
@@ -55,21 +58,62 @@ export function ProjectObjectList(props: Props) {
     return codes?.data?.find((code) => code.id.id === objectId)?.text[lang] ?? '';
   }
 
-  const projObjects = trpc.projectObject.getByProjectId.useQuery({
+  const projectObjects = trpc.projectObject.getByProjectId.useQuery({
     projectId: props.projectId,
     orderBy,
   });
+
+  const objectYears = useMemo(() => {
+    if (!projectObjects.data) return [];
+    return projectObjects.data.map((obj) => dayjs(obj.startDate).year());
+  }, [projectObjects.data]);
 
   return (
     <Box>
       <Box
         css={css`
+          position: sticky;
+          top: 0;
+          z-index: 1;
+          background-color: white;
           display: flex;
+          gap: 1rem;
           justify-content: flex-end;
           align-items: center;
           height: 80px;
         `}
       >
+        <FormControl size="small">
+          <InputLabel
+            css={css`
+              background-color: white;
+            `}
+            id="project-object-year-select-label"
+          >
+            {tr('projectObjectList.filterByStartYearLabel')}
+          </InputLabel>
+          <Select
+            css={css`
+              width: 200px;
+            `}
+            labelId="project-object-year-select-label"
+            value={byYear}
+            onChange={(e) => {
+              if (e.target.value === 'all') {
+                setByYear('all');
+              } else {
+                setByYear(Number(e.target.value));
+              }
+            }}
+          >
+            <MenuItem value={'all'}>{tr('projectObjectList.allYears')}</MenuItem>
+            {objectYears.map((year) => (
+              <MenuItem key={year} value={year}>
+                {year}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <FormControl
           size="small"
           css={css`
@@ -110,7 +154,7 @@ export function ProjectObjectList(props: Props) {
           {tr('projectObject.createNewBtnLabel')}
         </Button>
       </Box>
-      {projObjects.isLoading ? (
+      {projectObjects.isLoading ? (
         <Box
           css={css`
             display: flex;
@@ -122,55 +166,57 @@ export function ProjectObjectList(props: Props) {
           <Skeleton variant="rectangular" animation="wave" height={68} />
           <Skeleton variant="rectangular" animation="wave" height={68} />
         </Box>
-      ) : projObjects.data?.length === 0 ? (
+      ) : projectObjects.data?.length === 0 ? (
         <span>{tr('projectObjectList.noProjectObjects')}</span>
       ) : (
         <List>
-          {projObjects.data?.map((projObj) => (
-            <CardActionArea
-              key={projObj.projectObjectId}
-              component={Link}
-              to={`/${props.projectType}/${props.projectId}/kohde/${projObj.projectObjectId}`}
-            >
-              <Card variant="outlined" css={cardStyle}>
-                <NavigateNext sx={{ color: '#aaa', mr: 1 }} />
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '0.15rem',
-                  }}
-                >
-                  <Typography sx={{ lineHeight: '120%' }} variant="button">
-                    {projObj.objectName}
-                  </Typography>
+          {projectObjects.data
+            ?.filter((obj) => byYear === 'all' || dayjs(obj.startDate).year() === byYear)
+            .map((projObj) => (
+              <CardActionArea
+                key={projObj.projectObjectId}
+                component={Link}
+                to={`/${props.projectType}/${props.projectId}/kohde/${projObj.projectObjectId}`}
+              >
+                <Card variant="outlined" css={cardStyle}>
+                  <NavigateNext sx={{ color: '#aaa', mr: 1 }} />
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: '0.15rem',
+                    }}
+                  >
+                    <Typography sx={{ lineHeight: '120%' }} variant="button">
+                      {projObj.objectName}
+                    </Typography>
 
-                  <Typography sx={{ lineHeight: '120%' }} variant="overline">
-                    {dayjs(projObj.startDate).format(tr('date.format'))} —{' '}
-                    {projObj.endDate !== 'infinity' &&
-                      dayjs(projObj.endDate).format(tr('date.format'))}
-                  </Typography>
-                  {props.projectType === 'investointihanke' && (
-                    <span
-                      css={css`
-                        padding: 2px 6px;
-                        font-size: x-small;
-                        font-weight: 500;
-                        color: #333;
-                        border-radius: 8px;
-                        background-color: ${projObj.objectStage === '01' ? '#91c9ea' : '#f1eeeb'};
-                        position: absolute;
-                        bottom: 0.5rem;
-                        right: 1rem;
-                      `}
-                    >
-                      {getObjectStageTextById(projObj.objectStage)}
-                    </span>
-                  )}
-                </Box>
-              </Card>
-            </CardActionArea>
-          ))}
+                    <Typography sx={{ lineHeight: '120%' }} variant="overline">
+                      {dayjs(projObj.startDate).format(tr('date.format'))} —{' '}
+                      {projObj.endDate !== 'infinity' &&
+                        dayjs(projObj.endDate).format(tr('date.format'))}
+                    </Typography>
+                    {props.projectType === 'investointihanke' && (
+                      <span
+                        css={css`
+                          padding: 2px 6px;
+                          font-size: x-small;
+                          font-weight: 500;
+                          color: #333;
+                          border-radius: 8px;
+                          background-color: ${projObj.objectStage === '01' ? '#91c9ea' : '#f1eeeb'};
+                          position: absolute;
+                          bottom: 0.5rem;
+                          right: 1rem;
+                        `}
+                      >
+                        {getObjectStageTextById(projObj.objectStage)}
+                      </span>
+                    )}
+                  </Box>
+                </Card>
+              </CardActionArea>
+            ))}
         </List>
       )}
     </Box>
