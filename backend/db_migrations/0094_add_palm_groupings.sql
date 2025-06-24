62a84849d75e39ba285bb37daef6beb2eb25fedd
Atte <atte.peramaki@ubigu.fi>
2025-06-24 12:49:06 +0300

    add initial palm grouping migration

---
+++ backend/db_migrations/0094_add_palm_groupings.sql
@@ -0,0 +1,63 @@
+INSERT INTO
+    APP.code
+VALUES
+    (
+        ('PalmKoritus', '00'),
+        'PALM-koritus 0',
+        'PALM-koritus 0'
+    ),
+    (
+        ('PalmKoritus', '01'),
+        'PALM-koritus 1',
+        'PALM-koritus 1'
+    ),
+    (
+        ('PalmKoritus', '02'),
+        'PALM-koritus 2',
+        'PALM-koritus 2'
+    ),
+    (
+        ('PalmKoritus', '03'),
+        'PALM-koritus 3',
+        'PALM-koritus 3'
+    ),
+    (
+        ('PalmKoritus', '04'),
+        'PALM-koritus 4',
+        'PALM-koritus 4'
+    ),
+    (
+        ('PalmKoritus', '05'),
+        'PALM-koritus 5',
+        'PALM-koritus 5'
+    ),
+    (
+        ('PalmKoritus', '06'),
+        'PALM-koritus 6',
+        'PALM-koritus 6'
+    ),
+    (
+        ('PalmKoritus', '07'),
+        'PALM-koritus 7',
+        'PALM-koritus 7'
+    ),
+    (
+        ('PalmKoritus', '08'),
+        'PALM-koritus 8',
+        'PALM-koritus 8'
+    ),
+    (
+        ('PalmKoritus', '09'),
+        'PALM-koritus 9',
+        'PALM-koritus 9'
+    ),
+    (
+        ('PalmKoritus', '10'),
+        'PALM-koritus 10',
+        'PALM-koritus 10'
+    );
+
+-- ALTER TABLE
+--     app.
+-- ADD
+--     COLUMN target app.code_id DEFAULT ('PalmKoritus', '00');
