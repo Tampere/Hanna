511902790a12b9d925635c6bd1dcc225d6e09ef9
Atte <atte.peramaki@ubigu.fi>
2025-09-09 09:45:50 +0300

    Updated tests to reflect changes to Investment planning view

--- e2e/test/workTable.test.ts
+++ e2e/test/workTable.test.ts
@@ -6,9 +6,10 @@ test.describe('Work table with customized maximum row count', () => {
   test('should render without errors', async ({ testSession }) => {
     await testSession.page.getByRole('tab', { name: 'Ohjelmointi' }).click();
     const errorHeader = testSession.page.getByText('Unexpected Application Error!');
-    const header = testSession.page.getByTestId('worktable-title');
+    // Page header removed. check by content.
+    const pageContent = testSession.page.getByText('Näytä vain omat kohteet');
 
     await expect(errorHeader).not.toBeVisible();
-    await expect(header).toBeVisible();
+    await expect(pageContent).toBeVisible();
   });
 });
