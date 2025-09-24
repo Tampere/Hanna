b2ce8166092c195e1796f21c1d4ae5c96f9ea07d
Atte <atte.peramaki@ubigu.fi>
2025-09-24 17:06:00 +0300

    WBS now visible in tooltip

--- frontend/src/views/ProjectObject/SapWBSSelect.tsx
+++ frontend/src/views/ProjectObject/SapWBSSelect.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import { Autocomplete, CircularProgress, TextField, Tooltip } from '@mui/material';
 import { ControllerRenderProps, FieldValues } from 'react-hook-form';
 
 import { trpc } from '@frontend/client';
@@ -30,46 +30,56 @@ export function SapWBSSelect(props: Props) {
 
   return (
     <>
-      <Autocomplete
-        readOnly={props.readonlyProps?.InputProps?.readOnly ?? false}
-        autoHighlight
-        size="small"
-        ref={props.field.ref}
-        disabled={!isLoading && !options}
-        value={props.field.value ?? null}
-        onChange={(_, value) => props.field.onChange(value)}
-        options={options ?? []}
-        noOptionsText={tr('sapWBSSelect.noOptions')}
-        getOptionLabel={(id) => {
-          const wbs = wbsProjects.data?.find((wbs) => wbs.wbsId === id);
-          if (!wbs) return id;
-          return `${wbs?.shortDescription} (${wbs?.wbsId})`;
-        }}
-        renderInput={(params) => {
-          const { InputProps, ...restParams } = params;
-          return (
-            <>
-              <TextField
-                {...restParams}
-                InputProps={{
-                  ...InputProps,
-                  endAdornment: isLoading ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : (
-                    <>
-                      {!isLoading && !options && (
-                        <HelpTooltip title={tr('projectObject.sapWBSIdNoSAPProjectIdTooltip')} />
-                      )}
-                      {InputProps.endAdornment}
-                    </>
-                  ),
-                }}
-                {...props.readonlyProps}
-              />
-            </>
-          );
-        }}
-      />
+      <Tooltip
+        arrow
+        title={
+          props.field.value
+            ? `${wbsProjects.data?.find((wbs) => wbs.wbsId === props.field.value)
+                ?.shortDescription} (${props.field.value})`
+            : null
+        }
+      >
+        <Autocomplete
+          readOnly={props.readonlyProps?.InputProps?.readOnly ?? false}
+          autoHighlight
+          size="small"
+          ref={props.field.ref}
+          disabled={!isLoading && !options}
+          value={props.field.value ?? null}
+          onChange={(_, value) => props.field.onChange(value)}
+          options={options ?? []}
+          noOptionsText={tr('sapWBSSelect.noOptions')}
+          getOptionLabel={(id) => {
+            const wbs = wbsProjects.data?.find((wbs) => wbs.wbsId === id);
+            if (!wbs) return id;
+            return `${wbs?.shortDescription} (${wbs?.wbsId})`;
+          }}
+          renderInput={(params) => {
+            const { InputProps, ...restParams } = params;
+            return (
+              <>
+                <TextField
+                  {...restParams}
+                  InputProps={{
+                    ...InputProps,
+                    endAdornment: isLoading ? (
+                      <CircularProgress color="inherit" size={20} />
+                    ) : (
+                      <>
+                        {!isLoading && !options && (
+                          <HelpTooltip title={tr('projectObject.sapWBSIdNoSAPProjectIdTooltip')} />
+                        )}
+                        {InputProps.endAdornment}
+                      </>
+                    ),
+                  }}
+                  {...props.readonlyProps}
+                />
+              </>
+            );
+          }}
+        />
+      </Tooltip>
     </>
   );
 }
